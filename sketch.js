90fd2be272ffdc7f4124714265d4fd903d35cfe0
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-10-13 15:56:28 +0530

    Update sketch.js
    I have made some changes in the conditions in if statement.

--- sketch.js
+++ sketch.js
@@ -18,11 +18,11 @@ function draw() {
   {  
     car.velocityX=0;
     var deformation=0.5*weight*speed*speed/22500;
-    if (deformation<180){
+    if (deformation<100){
       car.shapeColor=color(0,255,0);
     }else if (deformation<180 && deformation>100){
       car.shapeColor=color(230,230,0);
-    }else if (deformation<180){
+    }else if (deformation>180){
       car.shapeColor=color(255,0,0);
     }
   }
